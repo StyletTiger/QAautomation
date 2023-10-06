8247269d73943e79853c4442a67b13392011527a
StyletTiger <stylettiger3004@gmail.com>
2023-10-06 17:40:03 +0300

    QA add

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-s = 'My Name is Julia'
-
-if 'Name' in s:
-    print('Substring found')
-
-index = s.find('Name')
-if index != -1:
-    print(f'Substring found at index {index}')
+import pytest
+pytest test_abs_project.py
 
 
 
